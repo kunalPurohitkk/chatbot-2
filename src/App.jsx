f223a3d465e39b393298ff03e85703ca650e1a33
Kunal Purohit <136796989+kunalPurohitkk@users.noreply.github.com>
2026-03-15 21:01:55 +0100

    Create main React component for the voicebot application

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to the Voicebot Application</h1>
+            {/* Add your voicebot functionality here */}
+        </div>
+    );
+};
+
+export default App;
